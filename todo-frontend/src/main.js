22a7655632a565e095d53908037cb1ba11bb3322
Sergej Gojkovic <programiranjepro@gmail.com>
2020-01-02 20:44:07 +0100

    Axios

--- todo-frontend/src/main.js
+++ todo-frontend/src/main.js
@@ -1,6 +1,8 @@
 import "@babel/polyfill";
 import "mutationobserver-shim";
 import Vue from "vue";
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 import "./plugins/bootstrap-vue";
 import "./plugins/bootstrap-vue";
 import App from "./App.vue";
@@ -8,6 +10,8 @@ import router from "./router";
 import store from "./store";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueAxios, axios);
 
 new Vue({
   router,
